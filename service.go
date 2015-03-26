3521a84e5ac96893912bfd399a247f38b78a6d96
Tom DeVito <tom@teltech.co>
2015-03-26 01:12:51 -0400

    Added env variable struct back in.

--- service.go
+++ service.go
@@ -9,45 +9,47 @@ import (
 type ServiceAPI interface {
 	List() (*ListServicesResponse, error)
 	Fetch(uuid string) (*Service, error)
+	Start(uuid string) (*Service, error)
 	Create(service *Service) (*Service, error)
+	Terminate(uuid string) (*Service, error)
 }
 
 type Service struct {
-	Uuid                 string           `json:"uuid,omitempty"`
-	ResourceUri          string           `json:"resource_uri,omitempty"`
-	Image                string           `json:"image,omitempty"`
-	ImageName            string           `json:"image_name,omitempty"`
-	ImageTag             string           `json:"image_tag,omitempty"`
-	Name                 string           `json:"name,omitempty"`
-	PublicDns            string           `json:"public_dns,omitempty"`
-	State                string           `json:"state,omitempty"`
-	Synchronized         bool             `json:"synchronized,omitempty"`
-	DeployedDateTime     string           `json:"deployed_datetime,omitempty"`
-	StartedDateTime      string           `json:"started_datetime,omitempty"`
-	StoppedDateTime      string           `json:"stopped_datetime,omitempty"`
-	DestroyedDateTime    string           `json:"destroyed_datetime,omitempty"`
-	TargetNumContainers  int32            `json:"target_num_containers,omitempty"`
-	CurrentNumContainers int32            `json:"current_num_containers,omitempty"`
-	RunningNumContainers int32            `json:"running_num_containers,omitempty"`
-	StoppedNumContainers int32            `json:"stopped_num_containers,omitempty"`
-	Stack                string           `json:"stack,omitempty"`
-	Containers           []string         `json:"containers,omitempty"`
-	ContainerPorts       []ContainerPorts `json:"container_ports,omitempty"`
-	ContainerEnvVars     []string         `json:"container_envvars,omitempty"`
-	Entrypoint           string           `json:"entrypoint,omitempty"`
-	RunCommand           string           `json:"run_command,omitempty"`
-	SequentialDeployment bool             `json:"sequential_deployment,omitempty"`
-	CpuShares            int32            `json:"cpu_shares,omitempty"`
-	Memory               int32            `json:"memory,omitempty"`
-	LinkedFromService    []LinkService    `json:"linked_from_service,omitempty"`
-	LinkedToService      []LinkService    `json:"linked_to_service,omitempty"`
-	AutoRestart          string           `json:"autorestart,omitempty"`
-	AutoDestroy          string           `json:"autodestroy,omitempty"`
-	AutoRedeploy         bool             `json:"autoredeploy,omitempty"`
-	Roles                []string         `json:"roles,omitempty"`
-	Privileged           bool             `json:"privileged,omitempty"`
-	DeploymentStrategy   string           `json:"deployment_strategy,omitempty"`
-	Tags                 []string         `json:"tags,omitempty"`
+	Uuid                 string            `json:"uuid,omitempty"`
+	ResourceUri          string            `json:"resource_uri,omitempty"`
+	Image                string            `json:"image,omitempty"`
+	ImageName            string            `json:"image_name,omitempty"`
+	ImageTag             string            `json:"image_tag,omitempty"`
+	Name                 string            `json:"name,omitempty"`
+	PublicDns            string            `json:"public_dns,omitempty"`
+	State                string            `json:"state,omitempty"`
+	Synchronized         bool              `json:"synchronized,omitempty"`
+	DeployedDateTime     string            `json:"deployed_datetime,omitempty"`
+	StartedDateTime      string            `json:"started_datetime,omitempty"`
+	StoppedDateTime      string            `json:"stopped_datetime,omitempty"`
+	DestroyedDateTime    string            `json:"destroyed_datetime,omitempty"`
+	TargetNumContainers  int32             `json:"target_num_containers,omitempty"`
+	CurrentNumContainers int32             `json:"current_num_containers,omitempty"`
+	RunningNumContainers int32             `json:"running_num_containers,omitempty"`
+	StoppedNumContainers int32             `json:"stopped_num_containers,omitempty"`
+	Stack                string            `json:"stack,omitempty"`
+	Containers           []string          `json:"containers,omitempty"`
+	ContainerPorts       []ContainerPorts  `json:"container_ports,omitempty"`
+	ContainerEnvVars     []ContainerEnvVar `json:"container_envvars,omitempty"`
+	Entrypoint           string            `json:"entrypoint,omitempty"`
+	RunCommand           string            `json:"run_command,omitempty"`
+	SequentialDeployment bool              `json:"sequential_deployment,omitempty"`
+	CpuShares            int32             `json:"cpu_shares,omitempty"`
+	Memory               int32             `json:"memory,omitempty"`
+	LinkedFromService    []LinkService     `json:"linked_from_service,omitempty"`
+	LinkedToService      []LinkService     `json:"linked_to_service,omitempty"`
+	AutoRestart          string            `json:"autorestart,omitempty"`
+	AutoDestroy          string            `json:"autodestroy,omitempty"`
+	AutoRedeploy         bool              `json:"autoredeploy,omitempty"`
+	Roles                []string          `json:"roles,omitempty"`
+	Privileged           bool              `json:"privileged,omitempty"`
+	DeploymentStrategy   string            `json:"deployment_strategy,omitempty"`
+	Tags                 []string          `json:"tags,omitempty"`
 	//Bindings
 	//LinkVariables
 }
@@ -59,6 +61,11 @@ type ContainerPorts struct {
 	PortName    string `json:"port_name,omitempty"`
 	Protocol    string `json:"protocol,omitempty"`
 	Published   bool   `json:"published,omitempty"`
+}
+
+type ContainerEnvVar struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
 }
 
 type LinkService struct {
@@ -104,5 +111,25 @@ func (me *serviceAPI) Create(service *Service) (*Service, error) {
 	if err := me.post("/service/", bytes.NewReader(serviceBytes), response); err != nil {
 		return nil, err
 	}
+
+	return response, nil
+}
+
+func (me *serviceAPI) Start(uuid string) (*Service, error) {
+	response := &Service{}
+	if err := me.post(fmt.Sprintf("/service/%s/start/", uuid), nil, response); err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
+
+func (me *serviceAPI) Terminate(uuid string) (*Service, error) {
+	response := &Service{}
+
+	if err := me.del(fmt.Sprintf("/service/%s/", uuid), response); err != nil {
+		return nil, err
+	}
+
 	return response, nil
 }
