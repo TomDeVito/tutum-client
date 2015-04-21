41fbc7cbb4f3f293b36c5ba086b0f70072909858
Tom DeVito <tom@teltech.co>
2015-04-21 16:08:24 -0400

    Added node cluster functionality.

--- node_cluster.go
+++ node_cluster.go
@@ -1,13 +1,25 @@
 package tutum
 
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+)
+
 type NodeClusterAPI interface {
 	List() (*ListNodeClusterResponse, error)
+	Create(nodeCluster *NodeCluster) (*NodeCluster, error)
+	Deploy(uuid string) (*NodeCluster, error)
+	Fetch(uuid string) (*NodeCluster, error)
 }
 
 type NodeCluster struct {
-	Name  string `json:"name,omitempty"`
-	Uuid  string `json:"uuid,omitempty"`
-	State string `json:"state,omitempty"`
+	Name           string `json:"name,omitempty"`
+	Uuid           string `json:"uuid,omitempty"`
+	Region         string `json:"region,omitempty"`
+	State          string `json:"state,omitempty"`
+	NodeType       string `json:"node_type,omitempty"`
+	TargetNumNodes int32  `json:"target_num_nodes,omitempty"`
 }
 
 type nodeClusterAPI struct {
@@ -25,5 +37,40 @@ func (me *nodeClusterAPI) List() (*ListNodeClusterResponse, error) {
 	if err := me.get("/nodecluster/", response); err != nil {
 		return nil, err
 	}
+	return response, nil
+}
+
+func (me *nodeClusterAPI) Create(nodeCluster *NodeCluster) (*NodeCluster, error) {
+	response := &NodeCluster{}
+
+	nodeClusterBytes, err := json.Marshal(nodeCluster)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := me.post("/nodecluster/", bytes.NewReader(nodeClusterBytes), response); err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
+
+func (me *nodeClusterAPI) Deploy(uuid string) (*NodeCluster, error) {
+	response := &NodeCluster{}
+
+	if err := me.post(fmt.Sprintf("/nodecluster/%s/deploy/", uuid), nil, response); err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
+
+func (me *nodeClusterAPI) Fetch(uuid string) (*NodeCluster, error) {
+	response := &NodeCluster{}
+
+	if err := me.get(fmt.Sprintf("/nodecluster/%s/", uuid), response); err != nil {
+		return nil, err
+	}
+
 	return response, nil
 }
